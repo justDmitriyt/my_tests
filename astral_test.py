d363b3860c8848992b3d83a836e854e1a1e40df9
justDmitriyt <124620535+justDmitriyt@users.noreply.github.com>
2024-12-28 17:36:00 +0400

    Add files via upload

--- astral_test.py
+++ astral_test.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 os.system('termux-camera-photo -c 1 storage/dcim/logs/logined.jpeg')
 torch = 0
 i = 0
@@ -11,7 +11,7 @@ while True:
     print("  \\__,_|___/\\__|_|  \\__,_|_|\033[0m")
     print()
     print('\033[34m\033[5mthis is not very useful staff, but i hipe you like it')
-    print('[1] torchok on//off\t[2] photik\t[1337] exit\n[3] battery statusisi')
+    print('[1] torchok on//off\t[2] photik\t[1337] exit\n[3] battery statusisi\t[4] geolocation\t[5] brightness\n[6] contact list\t[7] vibrate')
     print()
     while True:
         try:
@@ -58,5 +58,46 @@ while True:
         print(f'убиваемость {health}\nпироцентики {power}\n{"не всунуто" if plug.strip() == '"UNPLUGGED"' else "всунуто"}\nтемпературики {temp}')
         print()
         l = input('press enter to continue... ')
+    elif a == 4:
+        #print(os.system('termux-location'))
+        print('\033[31mnow this is not working')
+        l = input('press enter to continue... ')
+    elif a == 5:
+        while True:
+            b = input('write brightness (from 1 to 254)')
+            if b.isdigit() is True:
+                b = int(b)
+                if 0 < b and b < 255:
+                    os.system(f'termux-brightness {b}')
+                    break
+                else:
+                    print('retry')
+                    print()
+            else:
+                print('retry')
+                print()
+    elif a == 6:
+        os.system('termux-contact-list > contacts_list')
+        with open('contacts_list', 'r') as f:
+            spisok = f.read()[14:]
+            while spisok.find(':') != -1:
+                spisok = spisok[spisok.find(':') + 3:]
+                name = spisok[:spisok.find('"')]
+                spisok = spisok[spisok.find(':') + 3:]
+                nums = spisok[:spisok.find('"')]
+                print(f'contact: {name}\nnumber: {nums}')
+                print()
+        l = input('continue... ')
+    elif a == 7:
+        print()
+        print('[1] произвольная\t[2] sim sms\t[3] sim call')
+        vibr = int(input('input: '))
+        if vibr == 1:
+            durat = int(input('write duration in ms: '))
+            os.system(f'termux-vibrate -d {durat}')
+        elif vibr == 2:
+            for _ in range(2):
+                os.system('termux-vibrate -d 300')
+                #time.sleep(0.05)
     print('\033[0m\033[0m')
 os.system('clear')
---
+++ setup.sh
@@ -0,0 +1,8 @@
+apt update
+apt upgrade
+termux-setup-storage
+pkg install termux-api
+pkg install python
+cat astral_test.py > ~/astral_test.py
+clear
+echo "now write 'python astral_test.py' to open this shit!"
